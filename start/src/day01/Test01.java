b0e3fe90f8c2476363a32baf6e7ed09678026e08
y <y@DESKTOP-1E4JF9F>
2022-02-23 18:17:46 +0900

    1st commit

---
+++ start/src/day01/Test01.java
@@ -0,0 +1,21 @@
+package day01;
+
+public class Test01 {
+	public static void main(String[] args) {
+		System.out.println("아무거나"); // 처음에 문자열 출력
+		
+		Test01 t1 = new Test01();
+		
+		
+		t1.adc(); // Test01 클래스에 있는 abc() 실행하세요
+		
+		
+	}
+
+	public void adc() {}
+	
+	
+	// static 멤버는 static 영역에 클래스가 실행되는 순간 먼저 올려진다.
+	
+}
+ 
